a2aab628e0db57c526abbc8bea22ee0af577e98b
Maarten de Klerk <maarten.deklerk@gmail.com>
2018-01-22 16:17:59 +0100

    Added :email: mail support

--- dashboard/config.py
+++ dashboard/config.py
@@ -1,6 +1,11 @@
-class BaseConfig(object):
-    DEBUG = False
-
-
-class DevelopmentConfig(BaseConfig):
+class Config(object):
     DEBUG = True
+
+    # email server
+    MAIL_SERVER = 'smtp.gmail.com'
+    MAIL_PORT = 465
+    MAIL_USE_SSL = True
+    MAIL_USERNAME = ''
+    MAIL_PASSWORD = ''
+
+    ADMINS = ['maarten.deklerk@gmail.com']
--- dashboard/main.py
+++ dashboard/main.py
@@ -3,27 +3,29 @@ from flask import Flask, render_template, abort, Blueprint
 from jinja2 import TemplateNotFound
 import logging
 from logging.handlers import RotatingFileHandler
-from config import DevelopmentConfig
+from flask_mail import Mail, Message
+
+from config import Config
 
 from controller.rooms_controller import api_rooms_blueprint
 from controller.devices_controller import api_devices_blueprint
 
 app = Flask(__name__)
 
+app.config.from_object(Config)
+
 app.register_blueprint(api_rooms_blueprint, url_prefix="/api")
 app.register_blueprint(api_devices_blueprint, url_prefix="/api")
 
+mail = Mail(app)
+
 @app.route('/')
 def index():
+
     try:
         return render_template('index.html')
     except TemplateNotFound:
         abort(404)
 
 if __name__ == '__main__':
-    handler = RotatingFileHandler('foo.log', maxBytes=10000, backupCount=1)
-    handler.setLevel(logging.INFO)
-    app.logger.addHandler(handler)
-    app.debug = True
-
     app.run(host='0.0.0.0')
