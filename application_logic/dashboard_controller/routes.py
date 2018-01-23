7a2ba70572e2526089accd1f69165a8c539a9fdb
Maffia <displee@hotmail.com>
2018-01-23 12:07:23 +0100

    Merged.

--- application_logic/dashboard_controller/routes.py
+++ application_logic/dashboard_controller/routes.py
@@ -1,16 +1,101 @@
-from flask import render_template, abort
+import json
+
+from flask import render_template, abort, Response, request
 from jinja2 import TemplateNotFound
 
-from application_logic.dashboard_controller.devices_controller import api_devices_blueprint
-from application_logic.dashboard_controller.rooms_controller import api_rooms_blueprint
 from application_logic.dashboard_controller import app
-
-app.register_blueprint(api_rooms_blueprint, url_prefix="/api")
-app.register_blueprint(api_devices_blueprint, url_prefix="/api")
 
 @app.route('/')
 def index():
-    try:
-        return render_template('index.html')
-    except TemplateNotFound:
-        abort(404)
+    return render_template('index.html')
+
+
+@app.route('/api/users')
+def get_users():
+    return ''
+
+
+@app.route('/api/temperature/<string:city>')
+def temperature(city):
+    # TODO retrieve automatically temperature from argued city
+    data = 13
+    return Response(json.dumps(data), mimetype='application/json')
+
+
+@app.route('/api/sensors/active')
+def active_sensors():
+    # TODO retrieve all active sensors
+    data = 3
+    return Response(json.dumps(data), mimetype='application/json')
+
+
+@app.route('/api/damages')
+def water_damages():
+    # TODO retrieve all active sensors
+    damages = [0, 1, 2, 6, 8]
+    return Response(json.dumps(damages), mimetype='application/json')
+
+
+@app.route('/api/rooms/<int:room_id>/<string:sensor_id>', methods=['PUT'])
+def update_sensor_name(room_id, sensor_id):
+    form = request.form
+    if 'name' in form:
+        print('name found=' + form['name'])
+    elif 'active' in form:
+        print('active found=' + form['active'])
+
+    print(str(room_id) + ' -> ' + sensor_id)
+    # room = [room for room in rooms if room['id'] == room_id]
+    error = None
+    response = {
+        'error': error
+    }
+    return Response(json.dumps(response), mimetype='application/json')
+
+
+@app.route('/api/sensors/get')
+def get_unnamed_sensors():
+    # get all unnamed sensors from the database so user can fill name for them
+
+    # dummy data
+    sensors = [
+        {
+            'id': 1
+        }
+    ]
+    return Response(json.dumps(sensors), mimetype='application/json')
+
+
+@app.route('/api/rooms')
+def get_rooms():
+    rooms = [
+        {
+            'id': 3,
+            'name': 'Hallo jumbo'
+        },
+        {
+            'id': 5,
+            'name': 'Hoogvliet altijd nummer 1'
+        }
+    ]
+    return Response(json.dumps(rooms), mimetype='application/json')
+
+
+@app.route('/api/rooms/<string:room_id>/sensors')
+def get_room_sensors(room_id):
+    # TODO Get sensors for argued room id
+
+    # dummy data
+    sensors = [
+        {
+            'id': 'Hallo',
+            'name': 'Idk sensor',
+            'status': 0
+        },
+        {
+            'id': 'FF29DF',
+            'name': 'Tweede sensor',
+            'status': 1
+        }
+    ]
+    return Response(json.dumps(sensors), mimetype='application/json')
--- static/js/main.js
+++ static/js/main.js
@@ -1,4 +1,3 @@
-var editMode = false;
 
 var user_data = {
     room: -1,
@@ -8,64 +7,8 @@ var user_data = {
 
 $(function() {
     loop();
-    $('#edit-sensor-button').click(function () {
-        editMode = !editMode;
-        if (editMode) {
-            $(this).find('i').removeClass('fa-pencil-square-o').addClass('fa-check');
-            $('#sensors').find('div[data-role=collapsible]').removeClass('ui-disabled');
-        } else {
-            $(this).find('i').removeClass('fa-check').addClass('fa-pencil-square-o');
-            $('#sensors').find('div[data-role=collapsible]').addClass('ui-disabled');
-        }
-    });
-    $('#settings-rooms').find('.mobile-list').on('click', 'li a', function() {
-        var id = parseInt($(this).parent().attr('value'));
-        $('#room-title').text('Kamer ' + id);
-    });
-
-    $('#container').on('pagebeforeshow', 'div[data-role="page"]', function() {
-        var id = $(this).attr('id').replace('#', '');
-        console.log('switching to page=' + id);
-        switch(id) {
-            case 'home':
-                //TODO Refresh dahsboard via ajax
-                $('#sensor-count').text(random() + ' Sensoren actief');
-                $('#damage-occurrences').text(random());
-                $('#temperature').text(random() + '°C');
-                break;
-            case 'settings-rooms':
-                $.get('/api/rooms', function(json) {
-                    var list = $('#settings-rooms').find('.mobile-list');
-                    list.empty();
-                    for(var i in json) {
-                        var room = json[i];
-                        var item = $('<li value="' + room.id + '"><a href="#settings-room" data-transition="slide"><i class="fa fa-bed"></i>' + room.name + '</a></li>');
-                        list.append(item);
-                    }
-                    user_data.rooms = json;
-                    list.listview('refresh');
-                });
-                break;
-            case 'settings-room':
-                $.get('/api/rooms/' + user_data.room + '/sensors', function(json) {
-                    var list = $('#sensors');
-                    list.empty();
-                    for(var i in json) {
-                        var sensor = json[i];
-                        var status = sensor.status === 1 ? 'active' : 'inactive';
-                        var flipper = $('<input type="checkbox" data-role="flipswitch">');
-                        var item = $('<li><a data-transition="slide"><i class="fa fa-bed"></i>' + sensor.name + '</a></li>');
-                        item.append(flipper);
-                        //var item = $('<div class="ui-disabled" data-role="collapsible" data-collapsed-icon="carat-d" data-expanded-icon="carat-u" data-iconpos="right"><h3><i class="fa fa-plug sensor-' + status + '"></i> ' + sensor.name + '</h3><div class="ui-field-contain"><label>Naam:</label><input type="text" placeholder="Voer een naam in..." value="' + sensor.name + '"></div></div>');
-                        list.append(item);
-                        item.find('input').flipswitch();
-                    }
-                    user_data.sensors = json;
-                    list.listview("refresh");
-                });
-                break;
-        }
-    });
+    connectSmartCities();
+    bindEvents();
 });
 
 var nextColon = true;
@@ -95,6 +38,131 @@ function loop() {
     }, 1000);
 }
 
-function random() {
-    return parseInt(Math.random() * 10);
+function connectSmartCities() {
+    var config = {
+        apiKey: "AIzaSyBQGAOw3TcQOhHd6ZMnFX8HraBtCsKxB7o",
+        authDomain: "smartcities-d2e38.firebaseapp.com",
+        databaseURL: "https://smartcities-d2e38.firebaseio.com/",
+        storageBucket: "gs://smartcities-d2e38.appspot.com"
+    };
+    firebase.initializeApp(config);
+    var auth = firebase.auth();
+    auth.signInWithEmailAndPassword("admin@admin.nl", "adminadmin");
+    auth.onAuthStateChanged(function(user) {
+        if (user) {
+            var db = firebase.database().ref();
+            db.child("meldingen").child(user.uid).on('value', function (snapshot) {
+                snapshot.forEach(function(v) {
+                    var object = v.val();
+                    $.notify('<strong>' + v.key + '</strong><br/>Huidige regen status:' + object.regenStatus + '', { allow_dismiss: true });
+                });
+            });
+        }
+    });
+}
+
+function bindEvents() {
+    $('.header-dropdown ul').click(function() {
+        var menu = $(this).next();
+        var hidden = menu.css('display') === 'none';
+        if (hidden) {
+            menu.show();
+        } else {
+            menu.hide();
+        }
+    });
+    var popup = $('#sensor-popup');
+    $('#settings-rooms').find('.mobile-list').on('click', 'li a', function() {
+        var id = parseInt($(this).parent().attr('value'));
+        user_data.room = id;
+        $('#room-title').text('Kamer ' + id);
+    });
+    $('#sensors').on('click', 'li > a', function() {
+        var sensor = user_data.sensors[parseInt($(this).parent().attr('value'))];
+        $('#sensor-name').val(sensor.name);
+        popup.show();
+        popup.popup();
+        popup.popup("open");
+        user_data.sensor = sensor.id;
+    });
+    popup.find('button').click(function(e) {
+        $.ajax({
+            url: '/api/rooms/' + user_data.room + '/' + user_data.sensor,
+            type: 'PUT',
+            data: {
+                name: $('#sensor-name').val()
+            },
+            success: function(json) {
+                if (json.error) {
+                    $('#sensor-popup').find('.help-block').text(json.error);
+                } else {
+                    popup.popup("close");
+                }
+            }
+        });
+    });
+
+    $('#container').on('pagebeforeshow', 'div[data-role="page"]', function() {
+        var id = $(this).attr('id').replace('#', '');
+        console.log('switching to page=' + id);
+        switch(id) {
+            case 'home':
+                var city = 'Amersfoort';
+                //TODO Refresh dahsboard via ajax
+                $.get('/api/sensors/active', function(result) {
+                    $('#sensor-count').text(result + ' Sensoren actief');
+                });
+                $.get('/api/temperature/' + city, function(result) {
+                    $('#temperature').text(result + '°C');
+                });
+                $.get('/api/damages', function(json) {
+                    $('#damage-occurrences').text(json.length);
+                });
+                break;
+            case 'settings-rooms':
+                $.get('/api/rooms', function(json) {
+                    var list = $('#settings-rooms').find('.mobile-list');
+                    list.empty();
+                    for(var i in json) {
+                        var room = json[i];
+                        var item = $('<li value="' + room.id + '"><a href="#settings-room" data-transition="slide"><i class="fa fa-bed"></i>' + room.name + '</a></li>');
+                        list.append(item);
+                    }
+                    user_data.rooms = json;
+                    list.listview('refresh');
+                });
+                break;
+            case 'settings-room':
+                $.get('/api/rooms/' + user_data.room + '/sensors', function(json) {
+                    var list = $('#sensors');
+                    list.empty();
+                    for(var i in json) {
+                        var sensor = json[i];
+                        var status = sensor.status === 1 ? 'active' : 'inactive';
+                        var item = $('<li value="' + i + '"><a data-transition="slide"><i class="fa fa-plug sensor-' + status + '"></i>' + sensor.name + '</a><input type="checkbox" data-role="flipswitch"></li>');
+                        list.append(item);
+                        item.find('input').flipswitch();
+                        item.find('input').change(function() {
+                            var value = $(this).is(':checked');
+                            user_data.sensor = parseInt($(this).parent().parent().attr('value'));
+                            $.ajax({
+                                url: '/api/rooms/' + user_data.room + '/' + user_data.sensor,
+                                type: 'PUT',
+                                data: {
+                                    active: value
+                                },
+                                success: function(response) {
+                                    if (response.error) {
+                                        $.notify('<strong>Er is een fout opgetreden!</strong><br/>' + response.error);
+                                    }
+                                }
+                            });
+                        });
+                    }
+                    user_data.sensors = json;
+                    list.listview('refresh');
+                });
+                break;
+        }
+    });
 }
