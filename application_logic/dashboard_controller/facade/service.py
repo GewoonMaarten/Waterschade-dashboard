08014319590cfc78874373dc271ad3fe612dd028
Maarten de Klerk <maarten.deklerk@gmail.com>
2018-01-25 14:35:20 +0100

    Added dashboard controller service placeholder

---
+++ application_logic/dashboard_controller/facade/service.py
@@ -0,0 +1,14 @@
+
+
+class Service(object):
+    """Service for the sensor package to interact with"""
+
+    def __init__(self):
+        pass
+
+    def add_new_device(self, id, name=None):
+        pass
+    
+    def report_water_damage(self, id):
+        pass
+    
