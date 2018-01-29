9e7c0d4882169641506cad0476cdd22cd788f5a6
Nerinai <david.dm.dejong@student.hu.nl>
2018-01-29 15:52:31 +0100

    x

--- WifiService.py
+++ WifiService.py
@@ -1,5 +1,7 @@
 import os
 import sqlite3
+import random
+import string
 
 
 class WifiService:
@@ -23,8 +25,17 @@ class WifiService:
     def __del__(self):
         self.conn.close()
 
+def randomname(size = 64, chars=string.ascii_letters + string.digits):
+    return ''.join(random.choice(chars) for _ in range(size))
 
 service = WifiService()
 json = service.get_wifi_connection()
 
-print(json)
+try:
+    (os.remove("application_logic/sensor_controller/test.txt"))
+except OSError:
+    print("file not there")
+file_object = open("application_logic/sensor_controller/test.txt", 'w')
+
+file_object.write('\n'.join([json["ssid"], json["password"], randomname()]) + '\n')
+file_object.close()
