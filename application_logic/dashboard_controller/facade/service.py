cb87855f9f44ef859d9c89f87f48a6883c0a5fc2
Maarten de Klerk <maarten.deklerk@gmail.com>
2018-01-29 13:32:22 +0100

    Added placeholder functions to dashboard controller

--- application_logic/dashboard_controller/facade/service.py
+++ application_logic/dashboard_controller/facade/service.py
@@ -6,8 +6,14 @@ class Service(object):
     def __init__(self):
         pass
 
-    def add_new_device(self, id, name=None):
+    def add_new_device(self, id, name="water_sensor"):
         print(id, name)
 
     def report_water_damage(self, id):
         pass
+
+    def check_if_sensor_present(self, id):
+        # true or false
+        pass
+
+    
